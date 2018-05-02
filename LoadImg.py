13164b9af8c6b2993f520d184c0cebbf0cbf7075
wenwenlee <leejamesgm@gmail.com>
2018-05-02 15:53:13 +0800

    Update LoadImg.py

--- LoadImg.py
+++ LoadImg.py
@@ -4,24 +4,21 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 
-def load_data(FilePath):
+def load_Img(FilePath):
 
 	#读入文件列表，FilePath代表文件路径    
 	filelist = [os.path.join(FilePath, f) for f in os.listdir(FilePath)]    
 	
-	x_train = []   
-	y_train = []    
-	n1 = len(filelist)    
-	
+	x = []   
 	for img in filelist:   
 		#将图像格式转为numpy数组格式  
 		im = array(Image.open(img))
-		x_train.append(im)   
-
+		x.append(im)   
+		
+	return x
 	
-	
-	#print(x_train[2])
 if __name__ == "__main__":    
 
 	filePath = "D:\\code\\NotePad\\val_dir\\Cap"
-	load_data(filePath)
+	x_train = load_Img(filePath)
+	#print(x_train[2])
